8b96f3e8253f830a36db066ebb83d217ef22ee06
Ujjwal Bijalwan <ujjwalbijalwan589@gmail.com>
2025-05-19 12:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ function getInput()
 {
     const que = document.getElementsByClassName("question")[0].value;
     const names= (document.getElementsByClassName("names")[0].value).trim().split(/\s+/);
-console.log(que);
-console.log(names);
-console.log(names.length);
+
 var randomNumber = Math.floor(Math.random()*names.length);
 document.getElementsByClassName("que")[0].textContent=que;
 document.getElementsByClassName("name")[0].textContent=names[randomNumber];
